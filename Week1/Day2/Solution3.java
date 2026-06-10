b445352c8aeb1656e9a36535148f841700dd1bb8
shaileshpratapsingh132 <spsingh8115417704@gmail.com>
2026-06-10 23:29:41 +0530

    task2,task3 commit

---
+++ Week1/Day2/Solution3.java
@@ -0,0 +1,35 @@
+class Solution3 {
+
+    public int searchInsert(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+
+        Solution3 obj = new Solution3();
+
+        int[] nums = { 1, 3, 5, 6 };
+        int target = 2;
+
+        System.out.println(obj.searchInsert(nums, target));
+    }
+}
