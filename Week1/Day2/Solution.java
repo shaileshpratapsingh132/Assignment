00bf9f132e355dea44b2f134e287a0ee97d1509b
shaileshpratapsingh132 <spsingh8115417704@gmail.com>
2026-06-10 23:17:35 +0530

    day2 commit

---
+++ Week1/Day2/Solution.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int[] merged = new int[m + n];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+
+        while (i < m) {
+            merged[k++] = nums1[i++];
+        }
+
+        while (j < n) {
+            merged[k++] = nums2[j++];
+        }
+
+        int total = m + n;
+
+        if (total % 2 == 1) {
+            return merged[total / 2];
+        } else {
+            return (merged[total / 2 - 1] + merged[total / 2]) / 2.0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        int[] nums1 = { 1, 2 };
+        int[] nums2 = { 3, 4 };
+
+        double result = obj.findMedianSortedArrays(nums1, nums2);
+
+        System.out.println("Median = " + result);
+    }
+}
