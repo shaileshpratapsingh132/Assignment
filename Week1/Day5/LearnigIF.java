b48be5e42475397a30064db61de3efbf5aba5993
shaileshpratapsingh132 <spsingh8115417704@gmail.com>
2026-08-02 05:09:34 +0530

    LearnigIF

---
+++ Week1/Day5/LearnigIF.java
@@ -0,0 +1,12 @@
+public class LearnigIF {
+    public static void main(String[] args) {
+        boolean isMale = false;
+        String name = "Bob";
+        if (isMale) {
+            System.out.println("Mr." + name);
+        }
+        System.out.println("Wrong");
+
+    }
+
+}
