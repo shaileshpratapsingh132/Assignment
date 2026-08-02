e924cddf44ebad818520cf52c6a53b823a787e1e
shaileshpratapsingh132 <spsingh8115417704@gmail.com>
2026-08-03 00:34:55 +0530

    loop 500

--- Week1/Day5/Loop.java
+++ Week1/Day5/Loop.java
@@ -1,9 +1,15 @@
 public class Loop {
     public static void main(String[] args) {
-        int num = 1; // initialization
-        while (num <= 10) {
-            System.out.println("Loop" + num);
-            num = num + 1;
+        // int num = 1; // initialization
+        // while (num <= 10) {
+        // System.out.println("Loop" + num);
+        // num = num + 1;
+        // }
+        int count = 500;
+        while (count >= 200) {
+            System.out.println(count);
+            count = count - 1;
+
         }
     }
 
