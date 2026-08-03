05b88e9f27a1ea7f4e7127d97e23903d229d9234
shaileshpratapsingh132 <spsingh8115417704@gmail.com>
2026-08-04 02:25:39 +0530

    fuction

---
+++ Week1/Day5/Funtion.java
@@ -0,0 +1,11 @@
+public class Funtion {
+    public static void main(String[] args) {
+        greeting();
+
+    }
+
+    public static void greeting() {
+        System.out.println("Good Morning from Greater Noida");
+    }
+
+}
