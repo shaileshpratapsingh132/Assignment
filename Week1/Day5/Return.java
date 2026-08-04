2653cc07dba6987877fea6db21d7b58235dc381a
shaileshpratapsingh132 <spsingh8115417704@gmail.com>
2026-08-05 02:31:45 +0530

    return

---
+++ Week1/Day5/Return.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Return {
+    public static void main(String[] args) {
+        greet();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Calculator\n");
+        System.out.print("Please Enter the number");
+        int first = input.nextInt();
+        System.out.println("Please Enter the number");
+        int Second = input.nextInt();
+        int sum = first + Second;
+        System.out.println("Sum of the number is :" + sum);
+    }
+
+    public static
+
+    public static void greet() {
+        System.out.println("Welcom to calculator\n");
+    }
+
+}
