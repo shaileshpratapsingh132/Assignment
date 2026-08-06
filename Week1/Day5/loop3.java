545d33417637c7a10f5a92ae6eba68e56107f486
shaileshpratapsingh132 <spsingh8115417704@gmail.com>
2026-08-07 02:16:11 +0530

    even number

--- Week1/Day5/loop3.java
+++ Week1/Day5/loop3.java
@@ -1,6 +1,11 @@
 public class loop3 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i += 2) {
+        // for (int i = 1; i <= 10; i += 2) {
+        // System.out.println(i);
+        // }
+        for (int i = 2; i <= 20; i += 2)
+
+        {
             System.out.println(i);
         }
     }
