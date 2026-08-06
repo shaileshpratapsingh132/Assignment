4eecfdd7822fe28b072bcf7d36f36181f0e6a322
shaileshpratapsingh132 <spsingh8115417704@gmail.com>
2026-08-07 02:13:31 +0530

    loop2

---
+++ Week1/Day5/loop2.java
@@ -0,0 +1,10 @@
+public class loop2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("Shailesh pratap singh");
+        }
+
+    }
+
+}
