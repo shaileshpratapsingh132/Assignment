ac8b92941988185ac1409290283b55291942379f
shaileshpratapsingh132 <spsingh8115417704@gmail.com>
2026-08-08 03:11:39 +0530

    continue

---
+++ Week1/Day5/Breack.java
@@ -0,0 +1,12 @@
+public class Breack {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            if (i == 5) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+    }
+
+}
