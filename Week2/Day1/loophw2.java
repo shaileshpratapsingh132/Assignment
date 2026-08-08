77958fb6de9d98bbf3849e90158756eb0387046d
shaileshpratapsingh132 <spsingh8115417704@gmail.com>
2026-08-08 15:52:55 +0530

    loop problem solve

---
+++ Week2/Day1/loophw2.java
@@ -0,0 +1,13 @@
+package Week2.Day1;
+
+public class loophw2 {
+    public static void main(String[] args) {
+        // int n = 5;
+        for (int i = 5; i >= 1; i--)
+
+        {
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ Week2/Day1/loophw4.java
@@ -0,0 +1,10 @@
+package Week2.Day1;
+
+public class loophw4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Shailesh Pratap Singh");
+        }
+    }
+
+}
